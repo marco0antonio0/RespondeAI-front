29314c6713e455c74a46ffee672c94e2d13af931
marco0antonio0 <marcomesquitajr@hotmail.com>
2025-04-08 02:01:54 -0300

    feat/respondeai: atualizacao do projeto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import NavbarComponent from "@/components/head";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,14 +14,19 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-   <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-8 pb-20 gap-16 sm:py-0 font-[family-name:var(--font-geist-sans)]`}
-    >
-  <NavbarComponent/>
-  <Component {...pageProps} />
-  </div>
-  
-  </>;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-8 pb-20 gap-16 sm:py-0 font-[family-name:var(--font-geist-sans)]`}
+      >
+        <NavbarComponent />
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
+import Head from "next/head";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
